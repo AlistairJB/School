4b58b007cb9bc31179f1dc2e12c143b2f7632d09
s420369 <s420369@shhac150M15.ocsb.local>
2022-05-09 12:44:48 -0400

    chnage

--- list.py
+++
@@ -1,8 +0,0 @@
-alistairList = []#creates empty list
-
-alistairList2 = [1, "foo", 3.3, "w"] #creates list with elements
-
-print(alistairList) #prints entire list
-print(alistairList2) #prints entire list
-print(alistairList2[2])
-print(alistairList2[1:3]) #prints a range of elements
