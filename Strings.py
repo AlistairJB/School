f402f61fd0cc5d58fd4de6ffcd07ec199f343939
s420369 <s420369@shhac150M15.ocsb.local>
2022-05-10 12:25:22 -0400

    Pushed to Github

---
+++ Strings.py
@@ -0,0 +1,26 @@
+'''
+Strings are datatype
+Strings can be changed.
+String are written within single or double quotations
+All user inputs are Strings
+'''
+
+string1 = 'this is a string'
+print(string1)
+
+print(string1[5])
+print(string1[:6])
+print(string1[3:])
+print(string1[:])
+print(string1 * 2)
+
+var1 = ord('c')
+print(var1)
+
+var2 = chr(113)
+print(var2)
+
+'''
+for x in range(1,10):
+    print(string1[:x])
+'''
